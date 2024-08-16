e9c04054012a7806f399b2ee183c295d21e9ceab
satanakorn <satanakorn6.codercamp@gmail.com>
2024-08-16 14:20:41 +0700

    update

--- week1/lab1-3.ts
+++ week1/lab1-3.ts
@@ -1,5 +1,6 @@
 function removeItemFromArray(arr :number[]) {
     return arr.splice(1 , 1)
+    //return Array เพิ่มเติม return ตัวที่ส่งเข้ามาผ่านตัว parameter 
 }
 
 let arr: number[]= [10,30,20,50,40]
